ae1c1b878ad45539b7611e3bb5dd995c7815ae06
stevemoon <stevemoon@users.noreply.github.com>
2016-06-15 09:57:37 -0400

    refactored. works! submitted!

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -7,18 +7,11 @@ defmodule RunLengthEncoder do
   "1H1O1R1S1E" => "HORSE"
   """
   @spec encode(String.t) :: String.t
-  def encode(""), do: "" 
-  def encode(string), do:
-    ({first, rest} = String.next_codepoint(string)
-    _encode({first, String.next_codepoint(rest)}, 1, ""))
-    #defp _encode({cur, {nil}}, cur_count, accum), do:
-    #(accum <> cur_count <> cur)
-  defp _encode({cur, nil}, cur_count, accum), do:
-    (accum <> to_string(cur_count) <> cur)
-  defp _encode({cur, {next, rest}}, cur_count, accum) when cur == next, do:
-    _encode({next, String.next_codepoint(rest)}, cur_count + 1, accum)
-  defp _encode({cur, {next, rest}}, cur_count, accum), do:
-    _encode({next, String.next_codepoint(rest)}, 1, (accum <> to_string(cur_count) <> cur))
+  def encode(string), do: _encode(String.codepoints(string), 1, "")
+  defp _encode([], _count, accum), do: accum 
+  defp _encode([last], count, accum), do: accum <> to_string(count) <> last
+  defp _encode([cur, next | rest], count, accum) when cur == next, do: _encode([next | rest], count + 1, accum)
+  defp _encode([cur, next | rest], count, accum), do: _encode([next | rest], 1, accum <> to_string(count) <> cur)
 
   @spec decode(String.t) :: String.t
   def decode(string), do: _decode(Regex.scan(~r{(\d+)([A-Z])}, string), "")
