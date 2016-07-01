558d72d9d44bdeeb660f333d1af75959f24d241f
Steve Moon <steve.moon@gmail.com>
2016-06-30 23:54:26 -0400

    Working!

--- elixir/bracket-push/bracket_push.exs
+++ elixir/bracket-push/bracket_push.exs
@@ -3,20 +3,20 @@ defmodule BracketPush do
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
   @spec check_brackets(String.t) :: boolean
-  def check_brackets(""), do: :true
   def check_brackets(str) do
-    _chk_brkts(to_char_list(str), {0,0,0})
+    filtered = for x <- to_char_list(str), x in '[{()}]', do: x
+    _chk_brkts(filtered, [])
   end
-  defp _chk_brkts([], {0,0,0}), do: :true
-  defp _chk_brkts([], _), do: :false
-  defp _chk_brkts([?[ | tail], {square, brace, paren}), do: _chk_brkts(tail, {square + 1, brace, paren})
-  defp _chk_brkts([?{ | tail], {square, brace, paren}), do: _chk_brkts(tail, {square , brace + 1, paren})
-  defp _chk_brkts([?( | tail], {square, brace, paren}), do: _chk_brkts(tail, {square , brace, paren + 1})
-  defp _chk_brkts([?] | tail], {square, brace, paren}) when square > 0, do:
-    _chk_brkts(tail, {square - 1, brace, paren})
-  defp _chk_brkts([?} | tail], {square, brace, paren}) when brace > 0, do:
-    _chk_brkts(tail, {square, brace - 1, paren})
-  defp _chk_brkts([?} | tail], {square, brace, paren}) when paren > 0, do:
-    _chk_brkts(tail, {square, brace, paren - 1})
-  defp _chk_brkts([_ | tail], accum), do: _chk_brkts(tail, accum)
-end
+  
+  defp _chk_brkts([],[]), do: :true
+  defp _chk_brkts([head | tail], match_stack) when head in '[{(', do: 
+   _chk_brkts(tail, [_close_brkt(head) | match_stack]) 
+  defp _chk_brkts([head | tail], [match | rest]) when head == match, do:
+   _chk_brkts(tail, rest)
+  defp _chk_brkts(_, _), do: :false
+  
+  defp _close_brkt(?[), do: ?]
+  defp _close_brkt(?{), do: ?}
+  defp _close_brkt(?(), do: ?)
+  defp _close_brkt(_), do: :err_not_open_brkt
+  end
