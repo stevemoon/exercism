9a955da7a2db3f2b4c9174b23e70067e9cd60193
Steve Moon <steve.moon@gmail.com>
2016-07-07 15:46:54 -0400

    works

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -3,15 +3,37 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t
+  def verse(1) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+  def verse(2) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+  def verse(3) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
   def verse(number) do
-    # Your implementation here...
+    """
+    #{number - 1} bottles of beer on the wall, #{number - 1} bottles of beer.
+    Take one down and pass it around, #{number - 2} bottles of beer on the wall.
+    """
   end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t) :: String.t
-  def lyrics(range) do
-    # Your implementation here...
+  def lyrics(range \\ 100..1) do
+    Enum.reduce(range, "", fn x, acc -> acc <> "\n" <>  verse(x)  end) |>
+    String.trim_leading("\n")
   end
 end
