fb32f4815683097fa88acd9ac60d835edd1d35b8
Steve Moon <steve.moon@gmail.com>
2016-07-31 12:52:58 -0400

    all tests pass

--- elixir/grains/grains.exs
+++ elixir/grains/grains.exs
@@ -4,7 +4,7 @@ defmodule Grains do
   """
   @spec square(pos_integer) :: pos_integer
   def square(number) do
-
+    power(2, number - 1)
   end
 
   @doc """
@@ -12,6 +12,12 @@ defmodule Grains do
   """
   @spec total :: pos_integer
   def total do
-
+    power(2, 64) - 1 
   end
+
+  defp power(_, 0), do: 1
+  defp power(x, 1), do: x 
+  defp power(x, n) when n > 0, do: power(x, n, 1)
+  defp power(_, n, acc) when n == 0, do: acc
+  defp power(x, n, acc), do: power(x, n - 1, x * acc)
 end
