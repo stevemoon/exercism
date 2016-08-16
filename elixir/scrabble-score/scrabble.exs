351f060f91d40fa3a039d73510a99d33d0a0b8e0
Steve Moon <steve.moon@gmail.com>
2016-08-16 18:39:03 -0400

    all tests pass

--- elixir/scrabble-score/scrabble.exs
+++ elixir/scrabble-score/scrabble.exs
@@ -2,8 +2,23 @@ defmodule Scrabble do
   @doc """
   Calculate the scrabble score for the word.
   """
+
+  @letter_score %{"A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1,
+   "N" => 1, "R" => 1, "S" => 1, "T" => 1, "D" => 2, "G" => 2, "B" => 3, 
+   "C" => 3, "M" => 3, "P" => 3, "F" => 4, "H" => 4, "V" => 4, "W" => 4,
+   "Y" => 4, "K" => 5, "J" => 8, "X" => 8, "Q" => 10, "Z" => 10}
   @spec score(String.t) :: non_neg_integer
   def score(word) do
-
+    word
+    |> String.upcase
+    |> String.codepoints
+    |> score(0)
   end
+  defp score([], score), do: score
+  defp score([h | t], score) do 
+    case Map.has_key?(@letter_score, h) do
+      true -> score(t, score + @letter_score[h])
+      false -> score(t, score)
+  end
+end
 end
